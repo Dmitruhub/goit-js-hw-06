4fe44706c1ffb0d3521b36beb2710a8f9f7ead33
Dmitruhub <dmitrumail@gmail.com>
2022-09-23 23:33:17 +0300

    attempt 2

--- js/task-10.js
+++ js/task-10.js
@@ -1,9 +1,1 @@
-const input = document.querySelector('input');
-const buttonCreate = document.querySelector('[data-create]');
-const buttonDestroy = document.querySelector('[data-destroy]');
-const boxesAr = document.querySelector('#boxes');
 
-buttonCreate.addEventListener('click, inputValue');
-function inputValue(evt) {
-  const inputEl = input.value;
-}
